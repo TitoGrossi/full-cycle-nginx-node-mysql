10d191c9a08c22bc7adf9c180886c81bfc042403
TitoGrossi <tito.brossi@gmail.com>
2022-03-05 14:02:37 -0300

    Dockerize and wait, create on startup

--- node/index.js
+++ node/index.js
@@ -8,21 +8,35 @@ const config = {
     database: "nodedb",
 }
 
+const initialConnection = mysql.createConnection(config)
+initialConnection.query(`
+    CREATE TABLE IF NOT EXISTS person (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+    )
+`)
+initialConnection.end();
+
 const app = express();
 
 app.get('/', (_, res) => {
     var responseBody = "<p>full cycle rocks</p>"
     const connection = mysql.createConnection(config)
-    connection.query(`
-        CREATE TABLE IF NOT EXISTS person (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL
-        )
-    `)
+    // connection.query(`
+    //     CREATE TABLE IF NOT EXISTS person (
+    //         id INT AUTO_INCREMENT PRIMARY KEY,
+    //         name VARCHAR(255) NOT NULL
+    //     )
+    // `)
     connection.query(`
         INSERT INTO person(name) values('person')
     `);
     connection.query("SELECT * FROM person", (_, results) => {
+        if (results === undefined) {
+            responseBody += "<p>Still starting system, no results to be shown</p>"
+            res.send(responseBody)
+            return
+        }
         responseBody += "<p>Users in system:</p><ul>"
         results.map(person => responseBody += `<li>id: ${person.id}; name: ${person.name}</li>`)
         responseBody += "</ul>"
